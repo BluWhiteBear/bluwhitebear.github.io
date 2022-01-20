1290706badb16181299fbf0bcf9209203f388ac8
BluWhiteBear <bluwhitebear@gmail.com>
2022-01-20 05:21:29 -0500

    Updates

---
+++ precache-manifest.8bc311dccd0239094839a1d9a2ae021b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e70c3d47e238a699b23b923911825062",
+    "url": "/bluwhitebear.github.io/index.html"
+  },
+  {
+    "revision": "ce6e383462151b9e40b8",
+    "url": "/bluwhitebear.github.io/static/css/main.eba12994.chunk.css"
+  },
+  {
+    "revision": "c2462482f7daf1fc3ee8",
+    "url": "/bluwhitebear.github.io/static/js/2.9784b917.chunk.js"
+  },
+  {
+    "revision": "40f7fa7ab5996d1e53a916b11d456d6f",
+    "url": "/bluwhitebear.github.io/static/js/2.9784b917.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce6e383462151b9e40b8",
+    "url": "/bluwhitebear.github.io/static/js/main.6d8511a8.chunk.js"
+  },
+  {
+    "revision": "924614408d93a9ae3297",
+    "url": "/bluwhitebear.github.io/static/js/runtime-main.cab8b9d7.js"
+  }
+]);
